1f0c3dc197de76b49d97bef5c7fc25625e7c2432
Hidaayat <hidaayat07@gmail.com>
2023-05-30 15:42:47 +0200

    initial commit

--- electricity.dom.js
+++ electricity.dom.js
@@ -1,9 +1,81 @@
 // DOM element(s) references
 
-
-
+//getting references to various elements
+const topUpBtn = document.querySelector(".topupNow");
+const unitsAvailableDisplay = document.querySelector(".unitsAvailable")
+const totalAmountSpentDisplay = document.querySelector(".totalAmount");
+const totalUnits = document.querySelector(".totalUnits");
+const useBtn = document.querySelector(".useNow");
 // Factory Function instance 
 const electricity =  Electricity();
 
+// if localStorage doesn't exist create it and set it to the counts of the variables stored in the factory function
+
+if(!localStorage["unitsAvail"] && !localStorage["unitsBought"] && !localStorage["amountSpent"]) {
+    localStorage["unitsAvail"] = electricity.getUnitsAvailable();
+    localStorage["unitsBought"] = electricity.totalUnitsBought();
+    localStorage["amountSpent"] = electricity.totalAmountSpent();
+}
+
+// When localStorage has already been initialized run the factory function that resets the count
+// variables stored in the factory function to that of localStorage
+
+if (localStorage["unitsAvail"] && localStorage["unitsBought"] && localStorage["amountSpent"]) {
+    electricity.setLocalStorage();
+    unitsAvailableDisplay.innerText = localStorage["unitsAvail"];
+    totalAmountSpentDisplay.innerText = localStorage["amountSpent"];
+    totalUnits.innerText = localStorage["unitsBought"];
+
+}
 
 // DOM events here 
+
+function elecCalculate() {
+    
+    //get the checked button
+    const radioBtn = document.querySelector("input[name='buyElectricity']:checked");
+    //if button is checked store the selected button's id in a variable
+    if(radioBtn) {
+        var topUpSelected = radioBtn.id;
+    }
+    // check whether the selected buttons id equals a specific value and then call the topUpElectricity
+    //factory function with the right amount
+    if (topUpSelected === "ten") {
+        electricity.topUpElectricity(10);
+    } else if (topUpSelected === "twenty") {
+        electricity.topUpElectricity(20);
+    } else if (topUpSelected === "fifty") {
+        electricity.topUpElectricity(50)
+    } else if (topUpSelected === "advance") {
+        electricity.topUpElectricity("advance")
+    }
+
+
+    //update the DOM with the values that are returned from the factory function
+    unitsAvailableDisplay.innerText = electricity.getUnitsAvailable();
+    totalAmountSpentDisplay.innerText = electricity.totalAmountSpent();
+    totalUnits.innerText = electricity.totalUnitsBought();
+}
+
+function useElecAppliances() {
+
+    //get the checked radio button
+    const radioBtnAppliances = document.querySelector("input[name='useElectricity']:checked");
+
+
+    //if the button checked is true, get the value of the checked button
+    if(radioBtnAppliances) {
+        var applianceSelected = radioBtnAppliances.value;
+    }
+// run the factory function that uses applliances and pass in the value of the radio button selected
+    electricity.useAppliance(applianceSelected)
+
+    //update the dom for the units available that changed after using an appliance
+    unitsAvailableDisplay.innerText = electricity.getUnitsAvailable();
+
+}
+
+//add event listeners for the buttons being clicked and run the correct function
+
+topUpBtn.addEventListener("click", elecCalculate)
+useBtn.addEventListener("click", useElecAppliances)
--- electricity.js
+++ electricity.js
@@ -2,19 +2,78 @@ function Electricity() {
 
     // do we want to go with this or array? 
     let appliances = {
-        'Stove': 10, 
-        'Kettle': 5, 
-        'TV': 3, 
+        'Stove': 10,
+        'Kettle': 5,
+        'TV': 3,
         'Fridge': 13
     };
+// variables to store various counts
+    let elecTopUpAvailable = 0;
+
+    let unitsBought = 0;
+
+    let unitsAvailable = 0;
+
+    let advTaken;
+
+    let advancedOwed = 0;
+
+    // function topUpElectricity(amount) {
+    //     //if passed with advance run advance code
+    //     if (amount === "advance") {
+    //     // if there is no existing advance meaning false top up
+    //     // electricity with 30 and set advTaken variable to true
+    //         if (advTaken !== true) {
+    //             elecTopUpAvailable += 30;
+    //             advTaken = true;
+    //             advancedOwed = 30;
+    //         }
+
+    //         // else if not passed with advance, add the amount passed
+    //         // in to the elecTopUpAvailable variable
+    //     } else {
+    //         elecTopUpAvailable += amount;
+    //         //then if there is an existing advance to still be paid
+    //         // and the amount is more than 30, decrement the top up available by 30
+    //         if (advTaken === true && amount >= 30) {
+    //             elecTopUpAvailable -= 30;
+    //             advTaken = false;
+    //             advancedOwed -= 30;
+    //         } else if (advTaken === true && amount < 30) {
+    //             advancedOwed -= amount;
+    //         }
+    //     }
+
+    //     // convert the top up amount to units
+    //     unitsAvailable = elecTopUpAvailable * 0.7;
+    // }
 
     function topUpElectricity(amount) {
-
-
+ //checking if amount equals advance and advance hasn't been taken
+ 
+        if (amount === "advance" && !advTaken) {
+            //topup by 30 and set advTaken to true and advancedOwed to 30
+            elecTopUpAvailable += 30;
+            advTaken = true;
+            advancedOwed = 30;
+        } else if (advTaken && advancedOwed > 0) {
+            //else if advTaken is true and advanced owed is more than 0 subtract amount from advanced owed
+            advancedOwed -= amount;
+        } else if (!advTaken && advancedOwed <= 0) {
+            elecTopUpAvailable += amount;
+            advTaken = false;
+        }
+        //conversion of rand amounts into units
+        unitsBought = elecTopUpAvailable * 0.7;
+        unitsAvailable = unitsBought;
+        //setting localStorage to store values of count variables
+        localStorage["unitsAvail"] = unitsAvailable;
+        localStorage["unitsBought"] = unitsBought;
+        localStorage["amountSpent"] = elecTopUpAvailable;
     }
 
     function getUnitsAvailable() {
-         return unitsAvailable;
+        return unitsAvailable;
     }
 
     /*
@@ -22,16 +81,39 @@ function Electricity() {
     * other wise return false and do nothing.
     */
     function useAppliance(appliance) {
-        
+//looping through appliances object and checking
+        for (let app in appliances) {
+            // value passed in equals the same as appliance in object
+            if (appliance == app) {
+
+                //then subtract the value of the appliance number from unitsAvailable
+                unitsAvailable -= appliances[app];
+            }
+        }  
+// localStorage updated with value from unitsAvailable
+        localStorage["unitsAvail"] = unitsAvailable;
     }
+    
 
     function advanceTaken() {
+        return advTaken;
     }
-
+//returning amount spent
     function totalAmountSpent() {
+        return elecTopUpAvailable;
+    }
+// returning amount of units bought
+    function totalUnitsBought() {
+        return unitsBought;    
     }
 
-    function totalUnitsBought(){
+
+    // resetting the count variables to that of localStorage so that
+    //they are reflected upon page reload
+    function setLocalStorage() {
+        elecTopUpAvailable = Number(localStorage["amountSpent"])
+        unitsBought = Number(localStorage["unitsBought"])
+        unitsAvailable = Number(localStorage["unitsAvail"])
     }
 
     return {
@@ -40,7 +122,8 @@ function Electricity() {
         getUnitsAvailable,
         useAppliance,
         totalAmountSpent,
-        totalUnitsBought
+        totalUnitsBought,
+        setLocalStorage
 
     }
 }
